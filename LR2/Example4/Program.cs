4b207e772ac620e273c78f3bf192f988809939e2
Yehor Sadykov <97795698+yehorsadykov@users.noreply.github.com>
2024-09-17 19:13:15 +0300

    Add files via upload

---
+++ LR2/Example4/Program.cs
@@ -0,0 +1,21 @@
+﻿Random o = new Random();
+List<int> chisla = new List<int>();
+
+for (int i = 0; i < 10; i++) {
+   chisla.Add(o.Next(1, 10));
+   Console.WriteLine("{0}", chisla[i]);
+}
+
+Console.WriteLine();
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+for (int i = chisla.Count - 1; i >= 0; i--) {
+   if (chisla[i] == x) {
+      chisla.Remove(x);
+   }
+}
+
+for (int i = 0; i < chisla.Count; i++) {
+   Console.WriteLine("{0}", chisla[i]);
+}
---
+++ LR2/Example6/Program.cs
@@ -0,0 +1,19 @@
+﻿Random o = new Random();
+List<int> chisla = new List<int>();
+
+for (int i = 0; i < 10; i++) {
+   chisla.Add(o.Next(1, 20));
+   Console.WriteLine("{0}", chisla[i]);
+}
+
+Console.WriteLine();
+
+for (int i = chisla.Count - 1; i >= 0; i--) {
+   if (i % 2 == 0) {
+      chisla.RemoveAt(i);
+   }
+}
+
+for (int i = 0; i < chisla.Count; i++) {
+   Console.WriteLine("{0}", chisla[i]);
+}
