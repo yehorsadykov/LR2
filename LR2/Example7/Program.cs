6a558f77c068cdc957f11cda2f52b013cf6bacb3
Yehor Sadykov <97795698+yehorsadykov@users.noreply.github.com>
2024-09-17 19:16:38 +0300

    Add files via upload

---
+++ LR2/Example7/Program.cs
@@ -0,0 +1,19 @@
+﻿Random o = new Random();
+List<int> chisla = new List<int>();
+
+for (int i = 0; i < 10; i++) {
+   chisla.Add(o.Next(1, 10));
+   Console.WriteLine("{0}", chisla[i]);
+}
+
+Console.WriteLine("\nВведіть індекс: ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введіть число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+chisla.Insert(k,x);
+
+for (int i = 0; i < chisla.Count; i++) {
+   Console.WriteLine("{0}", chisla[i]);
+}
---
+++ LR2/Example8/Program.cs
@@ -0,0 +1,20 @@
+﻿Random o = new Random();
+List<int> chisla = new List<int>();
+
+for (int i = 0; i < 10; i++) {
+   chisla.Add(o.Next(1, 10));
+   Console.WriteLine("{0}", chisla[i]);
+}
+
+Console.WriteLine("Введіть число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+for (int i = chisla.Count - 1; i >= 0; i--) {
+   if (chisla[i] % 2 == 0) {
+      chisla.Insert(i, x);
+   }
+}
+
+for (int i = 0; i < chisla.Count; i++) {
+   Console.WriteLine("{0}", chisla[i]);
+}
